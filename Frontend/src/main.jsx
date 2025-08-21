4a3717beb0cb631dc288794c6d683ac547b4e1ef
Angie Yang <angel890817@hotmail.com>
2025-08-21 17:02:16 +1000

    Pages layout

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// main.jsx
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import './index.css';
+
+import App from './App.jsx';
+import Login from './components/Login.jsx';
+import Register from './components/Register.jsx';
+import Parking from './components/ParkingDashboard.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        {/* Redirect root to login page */}
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        {/* Layout wrapper */}
+        <Route element={<App />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          {/* Parking dashboard */}
+          <Route path="/parking" element={<Parking />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
