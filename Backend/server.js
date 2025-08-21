22edf77585deb78e22fb67988daf0742d4d0728f
Angie Yang <angel890817@hotmail.com>
2025-08-21 10:19:11 +1000

    models

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
